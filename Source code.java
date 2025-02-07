f3c6ec6f7d88920edd12c23977046053ea3b5434
Raghav Sharma <93008120+DevXRaghav@users.noreply.github.com>
2025-02-07 13:48:54 +0530

    Create Source code.java

---
+++ Source code.java
@@ -0,0 +1,9 @@
+package com.airline;
+
+public class BookingSystem {
+    public boolean bookFlight(int customerId, int flightId, String classType) {
+        // Add the booking logic here
+        System.out.println("Booking flight " + flightId + " for customer " + customerId);
+        return true;
+    }
+}
